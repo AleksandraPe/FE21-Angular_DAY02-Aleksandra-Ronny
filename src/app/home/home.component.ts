0f2a06fde9c929a8cc1155d434c37fd4e4c3b56b
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-03-23 14:29:15 +0100

    Maintenace Tab added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../car';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
--- src/app/maintain/maintain.component.ts
+++ src/app/maintain/maintain.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../car';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-maintain',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintainComponent implements OnInit {
 
+  car = new FormGroup({
+    brand: new FormControl("", Validators.required),
+    type:new FormControl("", Validators.required),
+    seats: new FormControl("", Validators.required),
+    gear: new FormControl("", Validators.required),
+    class: new FormControl("", Validators.required),
+    img: new FormControl("", Validators.required),
+  })
+
   constructor() { }
+
+  onSubmit(){
+    if (this.car.valid) {
+    }
+  }
 
   ngOnInit(): void {
   }
